dbb0e0b030cdbfbd897fbb657d21af8d13e52736
nourabdallah01 <nouraldin.s.abdallah@gmail.com>
2025-06-17 10:05:16 +0300

    End of Section 2

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,23 @@
+using System;
+using Core.Entities;
+using Infrastructure.config;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfigration).Assembly);
+    }
+}
+
+
